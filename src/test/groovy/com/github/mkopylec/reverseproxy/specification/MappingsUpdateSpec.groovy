f204381eef6e2e83c01f32ce90dc739fd4a3acf1
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2016-06-02 21:40:37 +0200

    mappings update scheduler test

---
+++ src/test/groovy/com/github/mkopylec/reverseproxy/specification/MappingsUpdateSpec.groovy
@@ -0,0 +1,33 @@
+package com.github.mkopylec.reverseproxy.specification
+
+import com.github.mkopylec.reverseproxy.BasicSpec
+import org.springframework.test.context.TestPropertySource
+
+import static com.github.mkopylec.reverseproxy.assertions.Assertions.assertThat
+import static org.springframework.http.HttpMethod.GET
+
+@TestPropertySource(
+        properties = ['reverse-proxy.mappings-update.on-non-http-error: false', 'reverse-proxy.mappings-update.interval-in-millis: 100']
+)
+class MappingsUpdateSpec extends BasicSpec {
+
+    def "Should update destination mappings when on non HTTP error while sending request to destination"() {
+        when:
+        sendRequest GET, '/uri/6/path/6'
+
+        then:
+        assertThat(localhost8080)
+                .haveReceivedRequest()
+                .withMethodAndUri(GET, '/path/6')
+
+        when:
+        updateMappingDestinations('/uri/6', 'localhost:8081')
+        sleep(150)
+        sendRequest GET, '/uri/6/path/6'
+
+        then:
+        assertThat(localhost8081)
+                .haveReceivedRequest()
+                .withMethodAndUri(GET, '/path/6')
+    }
+}
--- src/test/groovy/com/github/mkopylec/reverseproxy/specification/ProxyingRequestSpec.groovy
+++ src/test/groovy/com/github/mkopylec/reverseproxy/specification/ProxyingRequestSpec.groovy
@@ -120,4 +120,24 @@ class ProxyingRequestSpec extends BasicSpec {
         assertThat(response)
                 .hasStatus(INTERNAL_SERVER_ERROR)
     }
+
+    def "Should update destination mappings when on non HTTP error while sending request to destination"() {
+        when:
+        def response = sendRequest GET, '/uri/5/path/5'
+
+        then:
+        assertThat(localhost8080, localhost8081)
+                .haveReceivedNoRequest()
+        assertThat(response)
+                .hasStatus(INTERNAL_SERVER_ERROR)
+
+        when:
+        updateMappingDestinations('/uri/5', 'localhost:8080', 'localhost:8081')
+        sendRequest GET, '/uri/5/path/5'
+
+        then:
+        assertThat(localhost8080, localhost8081)
+                .haveReceivedRequest()
+                .withMethodAndUri(GET, '/path/5')
+    }
 }
