d2553def8b36a15697eac037fdf4f84a83e01525
Mariusz Kopylec <mariusz.kopylec@allegrogroup.com>
2017-01-05 14:55:44 +0100

    More detailed log if all retries fail

--- src/main/java/com/github/mkopylec/charon/core/retry/LoggingListener.java
+++ src/main/java/com/github/mkopylec/charon/core/retry/LoggingListener.java
@@ -33,7 +33,7 @@ public class LoggingListener extends RetryListenerSupport {
         Object mappingName = context.getAttribute(MAPPING_NAME_RETRY_ATTRIBUTE);
         if (mappingName != null) {
             if (throwable != null) {
-                log.error("All {} attempts to forward HTTP request using '{}' mapping has failed", context.getRetryCount(), mappingName);
+                log.error("All {} attempts to forward HTTP request using '{}' mapping has failed. {}", context.getRetryCount(), mappingName, retrieveErrorMessage(throwable));
             } else {
                 log.debug("Attempt {} to forward HTTP request using '{}' mapping has succeeded", context.getRetryCount() + 1, mappingName);
             }
