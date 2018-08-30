60be0d1050ed32b7a25bac70af0ed159ea2a35a0
Jean-Eric Cuendet <jean-eric.cuendet@vd.ch>
2018-08-30 15:57:33 +0200

    [Issue-65] Expose the HttpClient creation. Subclass can then modify HttpClient at will

--- src/main/java/com/github/mkopylec/charon/core/http/HttpClientProvider.java
+++ src/main/java/com/github/mkopylec/charon/core/http/HttpClientProvider.java
@@ -2,8 +2,8 @@ package com.github.mkopylec.charon.core.http;
 
 import com.github.mkopylec.charon.configuration.MappingProperties;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -11,25 +11,42 @@ import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.toMap;
-import static org.apache.http.impl.client.HttpClientBuilder.create;
 
 public class HttpClientProvider {
 
-    protected Map<String, RestOperations> httpClients = new HashMap<>();
+    protected Map<String, RestTemplate> httpClients = new HashMap<>();
 
     public void updateHttpClients(List<MappingProperties> mappings) {
-        httpClients = mappings.stream().collect(toMap(MappingProperties::getName, this::createHttpClient));
+        httpClients = mappings.stream().collect(toMap(MappingProperties::getName, this::createRestTemplate));
     }
 
-    public RestOperations getHttpClient(String mappingName) {
+    public RestTemplate getHttpClient(String mappingName) {
         return httpClients.get(mappingName);
     }
 
-    protected RestOperations createHttpClient(MappingProperties mapping) {
-        CloseableHttpClient client = create().useSystemProperties().disableRedirectHandling().build();
+    /**
+     * Can be overriden to modify the RestTemplate
+     *
+     * @param mapping
+     * @return
+     */
+    protected RestTemplate createRestTemplate(MappingProperties mapping) {
+        CloseableHttpClient client = createHttpClient(mapping).build();
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(client);
         requestFactory.setConnectTimeout(mapping.getTimeout().getConnect());
         requestFactory.setReadTimeout(mapping.getTimeout().getRead());
         return new RestTemplate(requestFactory);
+    }
+
+    /**
+     * Can be overriden to modify the HttpClient
+     *
+     * @param mapping
+     * @return
+     */
+    protected HttpClientBuilder createHttpClient(MappingProperties mapping/*can be used by overriden methods*/) {
+        final HttpClientBuilder builder = HttpClientBuilder.create();
+        builder.useSystemProperties().disableRedirectHandling();
+        return builder;
     }
 }
