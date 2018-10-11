21e88a5ae255c7fe73471c3e3af80618468c7866
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2018-10-11 17:59:34 +0200

    Customizable HTTP client in HttpClientProvider subclasses

--- src/main/java/com/github/mkopylec/charon/core/http/HttpClientProvider.java
+++ src/main/java/com/github/mkopylec/charon/core/http/HttpClientProvider.java
@@ -1,16 +1,18 @@
 package com.github.mkopylec.charon.core.http;
 
-import com.github.mkopylec.charon.configuration.MappingProperties;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.github.mkopylec.charon.configuration.MappingProperties;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
 import static java.util.stream.Collectors.toMap;
+import static org.apache.http.impl.client.HttpClientBuilder.create;
 
 public class HttpClientProvider {
 
@@ -24,12 +26,6 @@ public class HttpClientProvider {
         return httpClients.get(mappingName);
     }
 
-    /**
-     * Can be overriden to modify the RestTemplate
-     *
-     * @param mapping
-     * @return
-     */
     protected RestTemplate createRestTemplate(MappingProperties mapping) {
         CloseableHttpClient client = createHttpClient(mapping).build();
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(client);
@@ -38,15 +34,7 @@ public class HttpClientProvider {
         return new RestTemplate(requestFactory);
     }
 
-    /**
-     * Can be overriden to modify the HttpClient
-     *
-     * @param mapping
-     * @return
-     */
-    protected HttpClientBuilder createHttpClient(MappingProperties mapping/*can be used by overriden methods*/) {
-        final HttpClientBuilder builder = HttpClientBuilder.create();
-        builder.useSystemProperties().disableRedirectHandling();
-        return builder;
+    protected HttpClientBuilder createHttpClient(MappingProperties mapping) {
+        return create().useSystemProperties().disableRedirectHandling();
     }
 }
