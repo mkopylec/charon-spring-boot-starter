86837da9bf4379619fea60ecaa8d9078695b47ec
Mariusz Kopylec <mariusz.kopylec@allegro.pl>
2018-10-16 10:10:33 +0200

    Customizable HTTP client in HttpClientProvider subclasses

--- src/main/java/com/github/mkopylec/charon/core/http/HttpClientProvider.java
+++ src/main/java/com/github/mkopylec/charon/core/http/HttpClientProvider.java
@@ -1,36 +1,39 @@
 package com.github.mkopylec.charon.core.http;
 
+import com.github.mkopylec.charon.configuration.MappingProperties;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import com.github.mkopylec.charon.configuration.MappingProperties;
-import org.apache.http.impl.client.CloseableHttpClient;
-
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
 
 import static java.util.stream.Collectors.toMap;
 import static org.apache.http.impl.client.HttpClientBuilder.create;
 
 public class HttpClientProvider {
 
-    protected Map<String, RestOperations> httpClients = new HashMap<>();
+    protected Map<String, RestTemplate> httpClients = new HashMap<>();
 
     public void updateHttpClients(List<MappingProperties> mappings) {
-        httpClients = mappings.stream().collect(toMap(MappingProperties::getName, this::createHttpClient));
+        httpClients = mappings.stream().collect(toMap(MappingProperties::getName, this::createRestTemplate));
     }
 
-    public RestOperations getHttpClient(String mappingName) {
+    public RestTemplate getHttpClient(String mappingName) {
         return httpClients.get(mappingName);
     }
 
-    protected RestOperations createHttpClient(MappingProperties mapping) {
-        CloseableHttpClient client = create().useSystemProperties().disableRedirectHandling().build();
+    protected RestTemplate createRestTemplate(MappingProperties mapping) {
+        CloseableHttpClient client = createHttpClient(mapping).build();
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(client);
         requestFactory.setConnectTimeout(mapping.getTimeout().getConnect());
         requestFactory.setReadTimeout(mapping.getTimeout().getRead());
         return new RestTemplate(requestFactory);
+    }
+
+    protected HttpClientBuilder createHttpClient(MappingProperties mapping) {
+        return create().useSystemProperties().disableRedirectHandling();
     }
 }
