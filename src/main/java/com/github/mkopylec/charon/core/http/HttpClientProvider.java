f33479daee2b9c76755a0f778f552392bd2edeed
Jean-Eric Cuendet <jean-eric.cuendet@vd.ch>
2018-10-19 10:49:02 +0200

    [issue-69] Don't stock the cookies that are sent between the client and the browser

--- src/main/java/com/github/mkopylec/charon/core/http/HttpClientProvider.java
+++ src/main/java/com/github/mkopylec/charon/core/http/HttpClientProvider.java
@@ -1,18 +1,16 @@
 package com.github.mkopylec.charon.core.http;
 
+import com.github.mkopylec.charon.configuration.MappingProperties;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.github.mkopylec.charon.configuration.MappingProperties;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-
 import static java.util.stream.Collectors.toMap;
-import static org.apache.http.impl.client.HttpClientBuilder.create;
 
 public class HttpClientProvider {
 
@@ -26,6 +24,12 @@ public class HttpClientProvider {
         return httpClients.get(mappingName);
     }
 
+    /**
+     * Can be overriden to modify the RestTemplate
+     *
+     * @param mapping
+     * @return
+     */
     protected RestTemplate createRestTemplate(MappingProperties mapping) {
         CloseableHttpClient client = createHttpClient(mapping).build();
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(client);
@@ -34,7 +38,23 @@ public class HttpClientProvider {
         return new RestTemplate(requestFactory);
     }
 
-    protected HttpClientBuilder createHttpClient(MappingProperties mapping) {
-        return create().useSystemProperties().disableRedirectHandling();
+    /**
+     * Can be overriden to modify the HttpClient
+     *
+     * @param mapping
+     * @return
+     */
+    protected HttpClientBuilder createHttpClient(MappingProperties mapping/*can be used by overriden methods*/) {
+        final HttpClientBuilder builder = HttpClientBuilder.create();
+        builder
+                .useSystemProperties()
+                // Don't stock the cookies that are sent between the client and the browser
+                // This can cause a lot of problems around mixing Sessions between HTTP connections
+                // This can lead to Session steal from a user to another!
+                // Hint: Spring Cloud Hystrix also disables it by default
+                .disableCookieManagement()
+                // Tells HttpClient to not follow "302 Redirect" but to send them bac to the *browser*
+                .disableRedirectHandling();
+        return builder;
     }
 }
