79c49e5d87940be39ae2a8789a955615a385c090
mariusz.kopylec <mariusz.kopylec@o2.pl>
2021-05-04 14:12:06 +0200

    Encoded path supported in regex path rewriter

--- charon-spring-webflux/src/test/java/com/github/mkopylec/charon/test/RetryAttemptsResponseHeaderSetter.java
+++ charon-spring-webflux/src/test/java/com/github/mkopylec/charon/test/RetryAttemptsResponseHeaderSetter.java
@@ -1,17 +1,15 @@
 package com.github.mkopylec.charon.test;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-import reactor.core.publisher.Mono;
-
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeFunction;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.lang.String.valueOf;
 import static org.springframework.web.reactive.function.BodyExtractors.toDataBuffers;
-import static org.springframework.web.reactive.function.client.ClientResponse.from;
 
 class RetryAttemptsResponseHeaderSetter implements ExchangeFilterFunction {
 
@@ -21,7 +19,7 @@ class RetryAttemptsResponseHeaderSetter implements ExchangeFilterFunction {
     public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
         // Exceptions thrown before return WILL NOT trigger circuit breaker nor retryer.
         return next.exchange(request)
-                .map(response -> from(response)
+                .map(response -> response.mutate()
                         .headers(httpHeaders -> httpHeaders.set("Retry-Attempts", valueOf(attempt.incrementAndGet())))
                         .body(response.body(toDataBuffers())) // Need to consume body, otherwise the request will HANG after ReverseProxyFilter.
                         .build());
